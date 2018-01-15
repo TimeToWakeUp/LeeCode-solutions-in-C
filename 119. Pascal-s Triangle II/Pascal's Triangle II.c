201820a03857cdc6e93f7fcd5f055df1f5dc8e04
TimeToWakeUp <34735089+TimeToWakeUp@users.noreply.github.com>
2018-01-15 17:49:40 +0800

    源代码

--- 119. Pascal-s Triangle II/Pascal's Triangle II.c
+++ 119. Pascal-s Triangle II/Pascal's Triangle II.c
@@ -4,11 +4,11 @@
  */
 int* getRow(int rowIndex, int* returnSize) {
     int i,j;
-    int **result=(int **)malloc(sizeof(int*)*(rowIndex+1));
+    int **result=(int **)malloc(sizeof(int*)*(rowIndex+1)); //杨辉三角的行数
     
     for(i=0;i<=rowIndex;i++)
     {
-        *(result+i)=malloc(sizeof(int)*(i+1));
+        *(result+i)=malloc(sizeof(int)*(i+1)); //该行元素的个数
         for(j=0;j<=i;j++)
         {
             if(j==0||j==i)
