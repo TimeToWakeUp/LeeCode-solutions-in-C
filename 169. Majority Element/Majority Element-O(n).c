b29db73aff4335f6b6508361b1d1f51eb05c489e
TimeToWakeUp <34735089+TimeToWakeUp@users.noreply.github.com>
2018-01-18 15:41:25 +0800

    源代码

---
+++ 169. Majority Element/Majority Element-O(n).c
@@ -0,0 +1,15 @@
+int majorityElement(int* nums, int numsSize) {
+    int major=nums[0];
+    int count=1;
+    for(int i=1;i<numsSize;i++)
+    {
+        if(count==0)
+        {
+            count++;
+            major=nums[i];
+        }
+        else if(major==nums[i])  count++;
+        else count--;
+    }
+    return major;
+}
