8009ce7aa5806a86a69049cea961e78db532b322
TimeToWakeUp <34735089+TimeToWakeUp@users.noreply.github.com>
2018-01-20 15:29:46 +0800

    源代码

--- 189. Rotate Array/Rotate Array.c
+++ 189. Rotate Array/Rotate Array.c
@@ -5,4 +5,5 @@ void rotate(int* nums, int numsSize, int k) {
         numscopy[i]=nums[i];   //复制数组
     for(int i=0;i<numsSize;i++) 
         nums[(i+k)%numsSize]=numscopy[i];  //数组的下标为i的元素与其向右走k步的元素相等
+    free(numscopy);
 }
