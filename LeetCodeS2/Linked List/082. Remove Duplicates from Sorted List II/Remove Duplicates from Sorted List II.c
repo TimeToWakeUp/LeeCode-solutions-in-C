58383f862274d46b4b43594354d6cd812f79fe5b
TimeToWakeUp <34735089+TimeToWakeUp@users.noreply.github.com>
2018-03-13 22:52:46 +0800

    源代码

--- LeetCodeS2/Linked List/082. Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.c
+++ LeetCodeS2/Linked List/082. Remove Duplicates from Sorted List II/Remove Duplicates from Sorted List II.c
@@ -6,42 +6,24 @@
  * };
  */
 struct ListNode* deleteDuplicates(struct ListNode* head) {
-    if(head == NULL || head->next == NULL)  return head;
-    struct ListNode* pBehind = (struct ListNode*)malloc(sizeof(struct ListNode)); //引入头结点，则重复元素在链表的开头与在链表的中间两种情况
-    pBehind->val = INT_MAX;                                                       //处理方式一样，唯一不同的是重复元素在表头时要移动head指针
-    pBehind->next = head;
-    struct ListNode* pFront = head;
-    
-    
-    while(pFront->next != NULL)
-    {
-        //处理相邻的重复节点
-        if(pFront->val == pFront->next->val)
-        {
-            pFront = pFront->next->next;
-            if(pFront == NULL) //单独考虑重复元素在链表结尾的情况，如1-2-3-3
+    struct ListNode* pNew = (struct ListNode*)malloc(sizeof(struct ListNode));  //为了避免单独讨论链表为空的情况，故新开辟一个头结点
+        pNew->val = INT_MAX;
+        pNew->next = head;
+        struct ListNode* pNow = pNew;  //pNow用于遍历链表，是重复节点的前一个不重复的节点，如1-2-3-3-3，则pNow指向2
+        int duplicate;  //duplicate用于保存重复的节点的值
+        
+        while(pNow->next && pNow->next->next)  //遍历链表，因为pNow初始化为新开辟的头结点，则判断条件写为所示的这样。
+        {                                      //比如比较链表的第一个节点和第二个节点就得这样写
+            if(pNow->next->val == pNow->next->next->val)  //出现重复的节点
             {
-                if(pBehind->next == head)  return NULL;  //单独考虑重复元素同时在表头和表尾这种情况，如1-1
-                else
-                {
-                    pBehind->next = NULL;
-                    return head;
-                }
+                duplicate = pNow->next->val;  //保存重复的节点的值
+                while(pNow->next && pNow->next->val == duplicate)  //考虑到可能出现多个重复的节点，pNow是重复节点的前一个
+                    pNow->next = pNow->next->next;                 //不重复的节点，所以循环判断条件写为所示的这样
             }
-            while(pBehind->next->val == pFront->val)  
+            else  //没有出现重复的节点
             {
-                pFront = pFront->next;  //考虑到有可能连续出现多个重复的节点，如1-1-1-2-3
-                if(pFront == NULL)  break;
+                pNow = pNow->next;
             }
-            if(pBehind->next == head)  head = pFront; //如果重复元素出现在表头，则需要移动head指针
-            pBehind->next = pFront;
         }
-        else //没有出现相邻的重复节点
-        {
-            pFront = pFront->next;
-            pBehind = pBehind->next;
-        }
-        if(pFront == NULL)  return head;
-    }
-    return head;
+        return pNew->next;
 }
